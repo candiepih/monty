f7bd3379ee2b756130562c0c1f0c8cb29937953a
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-05-12 17:42:01 +0300

    Update instructions3.c

--- instructions3.c
+++ instructions3.c
@@ -53,12 +53,13 @@ void pchar(stack_t **stack, unsigned int line_number)
 {
 	int value;
 
-	if (!stack || !*stack)
+	if (!*stack)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		clear_stack(*stack);
 		clean_exit();
 	}
+
 	value = (*stack)->n;
 
 	if (value < 0 || value > 127)
@@ -80,8 +81,9 @@ void pchar(stack_t **stack, unsigned int line_number)
  */
 void pstr(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 {
-	stack_t *head = *stack;
+	stack_t *head
 
+	head = *stack;
 	while (head)
 	{
 		if (head->n == 0 || (head->n < 0 || head->n > 127))
