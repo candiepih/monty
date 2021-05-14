54fdf8eef977bf773f4dabec15d8fd305c460f36
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-05-14 10:20:59 +0300

    fixed stderr output incase of many arguments

--- monty.c
+++ monty.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: %s file\n", argv[0]);
+		fprintf(stderr, "USAGE: monty file\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 	handle_file(argv[1]);
